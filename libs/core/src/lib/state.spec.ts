68269db9959440fe058aa1d37e42fd41a9a472c0
Romain <rgeffrault@dood.com>
2026-02-14 18:44:51 +0100

    add spec for // state

--- libs/core/src/lib/state.spec.ts
+++ libs/core/src/lib/state.spec.ts
@@ -124,13 +124,19 @@ describe('state', () => {
 // todo implements the signature for parallel state
 describe('should allow to create multiple states in parallel with shared sources and methods', () => {
   it('expose a create method to generate parallel state', () => {
-    const myState = state({
-      method: (index: number) => index,
-      state: ({ params: index }) => ({
-        color: 'white',
-        index,
+    const myState = state(
+      {
+        method: (index: number) => index,
+        state: ({ params: index }) => ({
+          color: 'white',
+          index,
+        }),
+      },
+      ({ stateById }) => ({
+        changeColor: (id: number) =>
+          stateById.select(id)?.update((v) => ({ ...v, color: 'black' })),
       }),
-    });
+    );
 
     myState.create(0);
     myState.create(1);
@@ -139,41 +145,69 @@ describe('should allow to create multiple states in parallel with shared sources
     expect(myState.select(0)).toEqual({ color: 'white', index: 0 });
     expect(myState.select(1)).toEqual({ color: 'white', index: 1 });
     expect(myState.select(2)).toEqual({ color: 'white', index: 2 });
+
+    myState.changeColor(1);
+
+    expect(myState.select(0)).toEqual({ color: 'white', index: 0 });
+    expect(myState.select(1)).toEqual({ color: 'black', index: 1 });
+    expect(myState.select(2)).toEqual({ color: 'white', index: 2 });
   });
 
   it('should create a parallel state from a params', () => {
     const signalCurrentIndex = signal(0);
-    const myState = state({
-      params: signalCurrentIndex,
-      identifier: (index) => index,
-      state: ({ params: index }) => ({
-        color: 'white',
-        index,
+    const myState = state(
+      {
+        params: signalCurrentIndex,
+        identifier: (index) => index,
+        state: ({ params: index }) => ({
+          color: 'white',
+          index,
+        }),
+      },
+      ({ stateById }) => ({
+        changeColor: (id: number) =>
+          stateById.select(id)?.update((v) => ({ ...v, color: 'black' })),
       }),
-    });
+    );
 
     expect(myState(0)).toEqual({ color: 'white', index: 0 });
 
     signalCurrentIndex.set(1);
     expect(myState(1)).toEqual({ color: 'white', index: 1 });
+
+    myState.changeColor(1);
+    expect(myState(0)).toEqual({ color: 'white', index: 0 });
+    expect(myState(1)).toEqual({ color: 'black', index: 1 });
   });
 
   it('should generate parallel state from a signal list', () => {
     const indexList = signal([0, 1, 2]);
-    const myState3 = state({
-      from: indexList,
-      identifier: ({ item, index }) => index,
-      state: ({ params: { index } }) => ({
-        color: 'white',
-        index,
+    const myState3 = state(
+      {
+        from: indexList,
+        identifier: ({ item, index }) => index,
+        state: ({ params: { index } }) => ({
+          color: 'white',
+          index,
+        }),
+      },
+      ({ stateById }) => ({
+        changeColor: (id: number) =>
+          stateById.select(id)?.update((v) => ({ ...v, color: 'black' })),
       }),
-    });
+    );
 
     expect(myState3(0)).toEqual({ color: 'white', index: 0 });
     expect(myState3(1)).toEqual({ color: 'white', index: 1 });
     expect(myState3(2)).toEqual({ color: 'white', index: 2 });
 
     indexList.set([0, 1, 2, 3]);
+    expect(myState3(3)).toEqual({ color: 'white', index: 3 });
+
+    myState3.changeColor(1);
+    expect(myState3(0)).toEqual({ color: 'white', index: 0 });
+    expect(myState3(1)).toEqual({ color: 'black', index: 1 });
+    expect(myState3(2)).toEqual({ color: 'white', index: 2 });
     expect(myState3(3)).toEqual({ color: 'white', index: 3 });
   });
 
@@ -183,14 +217,20 @@ describe('should allow to create multiple states in parallel with shared sources
       1: 1,
       2: 2,
     });
-    const myState3 = state({
-      from: indexMap, // from only works with signal of list or object
-      identifier: ({ key, value }) => key,
-      state: ({ params: { key: index } }) => ({
-        color: 'white',
-        index,
+    const myState3 = state(
+      {
+        from: indexMap, // from only works with signal of list or object
+        identifier: ({ key, value }) => key,
+        state: ({ params: { key: index } }) => ({
+          color: 'white',
+          index,
+        }),
+      },
+      ({ stateById }) => ({
+        changeColor: (id: number) =>
+          stateById.select(id)?.update((v) => ({ ...v, color: 'black' })),
       }),
-    });
+    );
 
     expect(myState3(0)).toEqual({ color: 'white', index: 0 });
     expect(myState3(1)).toEqual({ color: 'white', index: 1 });
@@ -203,5 +243,42 @@ describe('should allow to create multiple states in parallel with shared sources
       3: 3,
     });
     expect(myState3(3)).toEqual({ color: 'white', index: 3 });
+
+    myState3.changeColor(1);
+    expect(myState3(0)).toEqual({ color: 'white', index: 0 });
+    expect(myState3(1)).toEqual({ color: 'black', index: 1 });
+    expect(myState3(2)).toEqual({ color: 'white', index: 2 });
+    expect(myState3(3)).toEqual({ color: 'white', index: 3 });
   });
+
+  // it('use a methodById that helps creating method on a state', () => {
+  //   const myState = state(
+  //     {
+  //       method: (index: number) => index,
+  //       state: ({ params: index }) => ({
+  //         color: 'white',
+  //         index,
+  //       }),
+  //     },
+  //     ({ stateById }) =>
+  //       methodById({
+  //         changeColor: (id: number) =>
+  //           stateById.select(id)?.update((v) => ({ ...v, color: 'black' })),
+  //       }),
+  //   );
+
+  //   myState.create(0);
+  //   myState.create(1);
+  //   myState.create(2);
+
+  //   expect(myState.select(0)).toEqual({ color: 'white', index: 0 });
+  //   expect(myState.select(1)).toEqual({ color: 'white', index: 1 });
+  //   expect(myState.select(2)).toEqual({ color: 'white', index: 2 });
+
+  //   myState.select(1).changeColor();
+
+  //   expect(myState.select(0)).toEqual({ color: 'white', index: 0 });
+  //   expect(myState.select(1)).toEqual({ color: 'black', index: 1 });
+  //   expect(myState.select(2)).toEqual({ color: 'white', index: 2 });
+  // });
 });
