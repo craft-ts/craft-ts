b8e9ed9fa9fb5ded940b36f22f7ba447e1aab436
Romain <rgeffrault@dood.com>
2026-04-03 11:27:56 +0200

    wip

--- apps/demo/src/app/conf.ts
+++ apps/demo/src/app/conf.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Equal, Expect } from 'test-type';
+import { UnionToTuple } from 'type-fest';
 
 @Component({
   selector: 'app-test',
@@ -9,9 +11,9 @@ import { Component } from '@angular/core';
 })
 export default class TestComponent {
   bingoGrid = [
-    ['Signals', 'Template', 'Router'],
-    ['Directive', 'Pipe', 'Service'],
-    ['NgZone', 'Inject', 'Standalone'],
+    ['Component', 'Defer', 'Observable'],
+    ['FormControl', '@for', 'ViewChild'],
+    ['Interceptor', 'OnPush', 'Hydration'],
   ];
 }
 
@@ -46,21 +48,25 @@ async function whatCanBeDerived() {
   //.  ^?
 
   type Name = string;
-  const _name: Name = 'Romain';
+  const _name: Name = 'Romain' as const;
   //.   ^?
   type _IsNameRomain = IsRomain<typeof _name>;
   //.  ^?
 
   type Square = object;
   const _firstSquare: Square = { word: 'Signals', checked: true };
-  type _FirstSquare = typeof _firstSquare;
-  //   ^?
+  //    ^?
   type _IsSquareMarked = IsSquareMarked<typeof _firstSquare>;
   //.  ^?
 }
 
 function case2() {
+  const _;
+
   type Name = string;
+  const _wrongName = 100 satisfies Name;
+  // ❌ Type 'number' does not satisfy the expected type 'string'.
+
   const _name = 'Romain' satisfies Name;
   //.   ^?
   type _IsNameRomain = IsRomain<typeof _name>;
@@ -74,27 +80,239 @@ function case2() {
   //.  ^?
 }
 
-function _bingoCase() {
-  const _player = {
-    player: {
-      name: 'Player 1',
-    },
-    bingoGrid: [
-      [
-        { word: 'Signals', checked: false },
-        { word: 'Template', checked: false },
-        { word: 'Router', checked: false },
-      ],
-      [
-        { word: 'Directive', checked: false },
-        { word: 'Pipe', checked: false },
-        { word: 'Service', checked: false },
-      ],
-      [
-        { word: 'NgZone', checked: false },
-        { word: 'Inject', checked: false },
-        { word: 'Standalone', checked: false },
-      ],
-    ],
+function f3() {
+  const _name = 'Romain';
+  //.   ^?
+  type _IsNameRomain = IsRomain<typeof _name>;
+  //.  ^?
+
+  type Square = object;
+  const _firstSquare = {
+    word: 'Signals',
+    checked: true,
+  } as const satisfies Square;
+  type _IsSquareMarked = IsSquareMarked<typeof _firstSquare>;
+  //.  ^?
+}
+function f4() {
+  type Square = {
+    word: string;
+    checked: boolean;
   };
+  const _firstSquare = {
+    word: 'Signals',
+    checked: true,
+  } satisfies Square;
+  type _IsSquareMarked = IsSquareMarked<typeof _firstSquare>;
+  //.  ^?
+}
+
+function f5() {
+  const r;
+
+  type Square = {
+    word: string;
+    checked: boolean;
+  };
+
+  function createSquare<T extends Square>(square: T): T {
+    // do some stuff
+    return square;
+  }
+
+  const _firstSquare = createSquare({ word: 'Signals', checked: true });
+  type _IsSquareMarked = IsSquareMarked<typeof _firstSquare>;
+  //.  ^?
+}
+
+function _bingoCase1() {
+  type PlayerCard = {
+    playerName: string;
+    bingoGrid: [
+      [string, string, string],
+      [string, string, string],
+      [string, string, string],
+    ]; // tuple
+  };
+
+  function bingoGame<
+    const AnnouncedWords extends string[],
+    const PlayerCards extends PlayerCard[],
+  >(_announcedWords: AnnouncedWords, _playerCards: PlayerCards): void {
+    // do some stuff
+    return {} as any;
+  }
+
+  const _game = bingoGame(
+    [
+      'Component', // 1 / 2
+      'Defer', // 1
+      'Observable', // 1 / 2
+      'FormControl', // 1 / 2
+      '@for', // 1
+      'ViewChild', // 1 / 2
+      'Input', // 2
+      'Output',
+    ],
+    [
+      {
+        playerName: 'Player 1',
+        bingoGrid: [
+          ['Component', 'Defer', 'Observable'],
+          ['FormControl', '@for', 'ViewChild'],
+          ['Interceptor', 'OnPush', 'Hydration'],
+        ],
+      },
+      {
+        playerName: 'Player 2',
+        bingoGrid: [
+          ['Component', 'Module', 'Observable'],
+          ['FormControl', 'HttpClient', 'ViewChild'],
+          ['Input', 'Service', 'OnInit'],
+        ],
+      },
+    ],
+  );
+
+  type _GameScoresWithoutWinner = Expect<
+    Equal<
+      typeof _game,
+      {
+        player1: 6;
+        player2: 5;
+      }
+    >
+  >;
+
+  // vitest: expectTypeOf(_game).toEqualTypeOf<{
+  //   player1: 6;
+  //   player2: 5;
+  // }>();
+
+  type BingoGridToUnionWords<BingoGrid extends PlayerCard['bingoGrid']> =
+    BingoGrid[number][number];
+
+  type _BingoGridToUnionTest = Expect<
+    Equal<
+      BingoGridToUnionWords<
+        [
+          ['Component', 'Defer', 'Observable'],
+          ['FormControl', '@for', 'ViewChild'],
+          ['Interceptor', 'OnPush', 'Hydration'],
+        ]
+      >,
+      | 'Component'
+      | 'Defer'
+      | 'Observable'
+      | 'FormControl'
+      | '@for'
+      | 'ViewChild'
+      | 'Interceptor'
+      | 'OnPush'
+      | 'Hydration'
+    >
+  >;
+
+  type ExtractAnnouncedWordsUnionInBingoGridUnion<
+    AnnouncedWordsUnion extends string,
+    BingoGridUnion extends string,
+  > = Extract<BingoGridUnion, AnnouncedWordsUnion>;
+
+  type _PickAnnouncedWordsInFlatBingoGridTest = Expect<
+    Equal<
+      ExtractAnnouncedWordsUnionInBingoGridUnion<
+        'Component' | 'Defer' | 'Observable',
+        'Component' | 'Defer' | 'Observable' | 'NotAnnounced'
+      >,
+      'Component' | 'Defer' | 'Observable'
+    >
+  >;
+
+  // UnionToTuple from type-fest
+  type GetBingoScore<
+    AnnouncedWords extends string[],
+    BingoGrid extends PlayerCard['bingoGrid'],
+  > = UnionToTuple<
+    ExtractAnnouncedWordsUnionInBingoGridUnion<
+      AnnouncedWords[number],
+      BingoGridToUnionWords<BingoGrid>
+    >
+  >['length'];
+
+  type ScoreShouldBe2 = Expect<
+    Equal<
+      GetBingoScore<
+        ['word1', 'word2', 'anotherWord'],
+        [
+          ['word1', 'word2', 'Observable'],
+          ['FormControl', '@for', 'ViewChild'],
+          ['Interceptor', 'OnPush', 'Hydration'],
+        ]
+      >,
+      2
+    >
+  >;
+}
+
+function _bingoCase2() {
+  type PlayerCard = {
+    playerName: string;
+    bingoGrid: [
+      [string, string, string],
+      [string, string, string],
+      [string, string, string],
+    ]; // tuple
+  };
+
+  function bingoGame<
+    const AnnouncedWords extends string[],
+    const PlayerCards extends PlayerCard[],
+  >(_announcedWords: AnnouncedWords, _playerCards: PlayerCards): void {
+    // do some stuff
+  }
+
+  const _game = bingoGame(
+    [
+      'Component',
+      'Defer',
+      'Observable',
+      'FormControl',
+      '@for',
+      'ViewChild',
+      'Input',
+      'Output',
+      'Interceptor',
+      'OnPush',
+      'Hydration',
+    ],
+    [
+      {
+        playerName: 'Player 1',
+        bingoGrid: [
+          ['Component', 'Defer', 'Observable'],
+          ['FormControl', '@for', 'ViewChild'],
+          ['Interceptor', 'OnPush', 'Hydration'],
+        ],
+      },
+      {
+        playerName: 'Player 2',
+        bingoGrid: [
+          ['Component', 'Module', 'Observable'],
+          ['FormControl', 'HttpClient', 'ViewChild'],
+          ['Input', 'Service', 'OnInit'],
+        ],
+      },
+    ],
+  );
+
+  type _GameScoresWithoutWinner = Expect<
+    Equal<
+      typeof _game,
+      {
+        player1: 9;
+        player2: 5;
+        winner: 'Player 1';
+      }
+    >
+  >;
 }
