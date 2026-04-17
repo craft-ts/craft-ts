5b0fdd98fd84221ba2647856eb3266712242473b
Romain <rgeffrault@dood.com>
2026-04-17 23:44:02 +0200

    WIP

--- libs/core/src/lib/service.spec.ts
+++ libs/core/src/lib/service.spec.ts
@@ -373,9 +373,9 @@ describe.todo('yield typage'); // with option like skipHost/optional
 // todo renvoyer une erreur si un yield d'un service à fournir est fait dans un service global (pas de host pour résoudre la dépendance)
 
 // todo inject/...toYield avec second paramètre pour savoir et exposer uniquement ce qu'il y a beosin
-// const counter = yield* CounterToYield(function* ({increment, state}) => {
-// const increment = yield* increment;
-// const state = yield* state;
+// const counter = yield* CounterToYield(function* ({increment, state, decrement}) => {
+// const useDecrement = yield* decrement();
+// triggerDecrementObservable.subscribe(() => useDecrement());
 //. return {increment, state}
 // });
 //});
@@ -388,3 +388,5 @@ describe.todo(
 );
 
 describe.todo('enable inject options'); // handle optional params to expose....
+
+// todo queryparams, penser à des Symbol qui force à faire des merges, et pas à spread pour qu'on puisse les garder et les concaténer ?
