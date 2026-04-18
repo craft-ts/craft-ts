d17ca0ba89602640470b8092daba798c5a5bb87e
Romain <rgeffrault@dood.com>
2026-04-18 19:10:20 +0200

    wip

--- libs/core/src/lib/service.spec.ts
+++ libs/core/src/lib/service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { state } from './state';
 import { signal } from '@angular/core';
+import { Subject } from 'rxjs';
 
 // todoBefore name du service faire que ce soit un object name/scope pour éviter des galères de typage poiur commencer
 // todoBefore analyser les testes pour les corriger si besoin
@@ -67,6 +68,8 @@ describe.todo('scope', () => {
     });
   });
 
+  // todo global service should not expose provideService
+
   it('should enable to create a global service by passing a name/scope', () => {
     const { injectCounter } = service(
       { name: 'Counter', scope: 'global' },
@@ -113,11 +116,103 @@ describe.todo('scope', () => {
     });
   });
 
-  // ? Ne prend pas en compte: with scope function can yield inside, and his scope become 'local' if their is a yield of a service toProvide,
+  it('should enable to create an abstract service by passing a name/scope', () => {
+    interface Counter {
+      (): number;
+      increment(): void;
+    }
+    const { injectCounter, provideCounter } = service(
+      { name: 'Counter', scope: 'abstract' },
+      abstract<Counter>(), // todo create abstract helper that just return the type and do nothing else, to be used for abstract service
+    );
+
+    expectTypeOf(injectCounter).toEqualTypeOf<() => Counter>();
+    expect(injectCounter).toBeDefined();
+
+    //@ts-expect-error provideCounter should not be defined because it's an abstract service, an implementation service should provide through requirement CounterRequirement
+    expect(provideCounter).toBeDefined();
+  });
+
+  it('should enable to create a service from an abstract service through requirement (It should provide the implementation service and abstract service)', () => {
+    const { injectCounter, CounterRequirement } = service(
+      { name: 'Counter', scope: 'abstract' },
+      abstract<{
+        (): number;
+        increment(): void;
+      }>(), // todo create abstract helper that just return the type and do nothing else, to be used for abstract service
+    );
+
+    // todo CounterRequirement should only be exposed when scope: 'abstract' is set
+
+    // todo when creating from requirement: CounterRequirement it should not be possible to create a global (to force to provide it ?) non
+    const { injectCounterImpl, provideCounterImpl } = service(
+      {
+        name: 'CounterImpl',
+        scope: 'toProvide',
+        requirement: CounterRequirement,
+      },
+      () => state(0, ({ update }) => ({ increment: update((v) => v + 1) })),
+    );
+
+    // todo provideCounterImpl should provide CounterImpl and the source of CounterRequirement
+
+    TestBed.configureTestingModule({
+      providers: [provideCounterImpl()],
+    });
+
+    TestBed.runInInjectionContext(() => {
+      const counter = injectCounter();
+      expect(counter()).toBe(0);
+      counter.increment();
+      expect(counter()).toBe(1);
+
+      const counterImpl = injectCounterImpl();
+      expect(counterImpl()).toBe(1);
+    });
+  });
+
+  it('should not enable to create a global service from an abstract service', () => {
+    const { CounterRequirement } = service(
+      { name: 'Counter', scope: 'abstract' },
+      abstract<{
+        (): number;
+        increment(): void;
+      }>(),
+    );
+
+    //@ts-expect-error it should not be possible to create a global service from an abstract service, it should force to provide an implementation
+    const { injectCounterImpl, provideCounterImpl } = service(
+      {
+        name: 'CounterImpl',
+        scope: 'global',
+        requirement: CounterRequirement,
+      },
+      () => state(0, ({ update }) => ({ increment: update((v) => v + 1) })),
+    );
+  });
+
+  it('should not enable to create a service implementation from an abstract service if the requirement is not satisfied', () => {
+    const { CounterRequirement } = service(
+      { name: 'Counter', scope: 'abstract' },
+      abstract<{
+        (): number;
+        increment(): void;
+      }>(),
+    );
+
+    //@ts-expect-error it should not be possible to create a service if the requirement is not satisfied,
+    const { injectCounterImpl, provideCounterImpl } = service(
+      {
+        name: 'CounterImpl',
+        scope: 'global',
+        requirement: CounterRequirement,
+      },
+      () => state(0),
+    );
+  });
 });
 
 describe('injectService should enable to binding inputs', () => {
-  // todo convention à la MaybeSignal (créer un utilitaire dédié ?)
   it('should enable to bind a signal input', () => {
     const { injectCounter } = service(
       { name: 'Counter', scope: 'global' },
@@ -134,6 +229,29 @@ describe('injectService should enable to binding inputs', () => {
     TestBed.runInInjectionContext(() => {
       // todo make a test that force to pass an input, and if it's not passed, throw an error
       const counter = injectCounter({ initialValue: 0 });
+      expect(counter()).toBe(0);
+      counter.increment();
+      expect(counter()).toBe(1);
+    });
+  });
+
+  it('should enable to bind an optional signal input and not bind an optional input', () => {
+    const { injectCounter } = service(
+      { name: 'Counter', scope: 'global' },
+      // ! inputs can only be set in the first params
+
+      (inputs: {
+        initialValue: MaybeSignal<number>; // todo create MaybeSignal
+        optionalProperty1?: MaybeSignal<number>; // todo create MaybeSignal
+        optionalProperty2?: MaybeSignal<number>; // todo create MaybeSignal
+      }) =>
+        state(toValue(inputs.initialValue), ({ update }) => ({
+          increment: update((v) => v + 1),
+        })), // todo create toValue helper
+    );
+
+    TestBed.runInInjectionContext(() => {
+      const counter = injectCounter({ initialValue: 0, optionalProperty1: 0 });
       expect(counter()).toBe(0);
       counter.increment();
       expect(counter()).toBe(1);
@@ -284,6 +402,44 @@ describe('serviceToYield should enable to binding inputs', () => {
     });
   });
 
+  it('should enable to bind an optional input and not bind an optional input', () => {
+    const { CounterToYield } = service(
+      'Counter',
+      (inputs: {
+        initialValue: MaybeSignal<number>;
+        optionalProperty1?: MaybeSignal<number>; // todo create MaybeSignal
+        optionalProperty2?: MaybeSignal<number>; // todo create MaybeSignal
+      }) =>
+        state(toValue(inputs.initialValue), ({ update }) => ({
+          increment: update((v) => v + 1),
+        })),
+    );
+
+    const { injectCounterExtended } = service('CounterExtended', function* () {
+      const counter = yield* CounterToYield({
+        initialValue: signal(10),
+        optionalProperty1: signal(20),
+      });
+
+      return {
+        ...counter,
+        incrementTwice: () => {
+          counter.increment();
+          counter.increment();
+        },
+      };
+    });
+
+    TestBed.runInInjectionContext(() => {
+      const counter = injectCounterExtended();
+      expect(counter()).toBe(10);
+      counter.increment();
+      expect(counter()).toBe(11);
+      counter.incrementTwice();
+      expect(counter()).toBe(13);
+    });
+  });
+
   it('should return a string as an error "Inputs Error, xxx is not provided" if an input is not provided or blocks the yield', () => {
     const { CounterToYield } = service(
       'Counter',
@@ -296,6 +452,7 @@ describe('serviceToYield should enable to binding inputs', () => {
     );
 
     const { injectCounterExtended } = service('CounterExtended', function* () {
+      //@ts-expect-error it should not be possible to yield if an input is not provided, it should throw an error or return a string error
       const counter = yield* CounterToYield();
 
       // The aim is to show that something went wrong because the input is not provided
@@ -361,16 +518,200 @@ describe('serviceToYield should enable to binding inputs', () => {
     });
   });
 
-  // todo cas où on yield 2 fois un service fonction (ça devrait faire 2 instances différentes, mais avec les mêmes inputs)
+  it('should enable to yield a service with the scope function several times that will generate different instances', () => {
+    const { CounterToYield } = service(
+      { name: 'Counter', scope: 'function' },
+      (inputs: { initialValue: MaybeSignal<number> }) =>
+        state(toValue(inputs.initialValue), ({ update }) => ({
+          increment: update((v) => v + 1),
+        })),
+    );
+
+    const { injectCounterExtended } = service('CounterExtended', function* () {
+      const counter1 = yield* CounterToYield({
+        initialValue: signal(10),
+      });
+      const counter2 = yield* CounterToYield({
+        initialValue: signal(20),
+      });
+
+      return {
+        counter1,
+        counter2,
+      };
+    });
+
+    TestBed.runInInjectionContext(() => {
+      const counterHandler = injectCounterExtended();
+      expect(counterHandler.counter1()).toBe(10);
+      counterHandler.counter1.increment();
+      expect(counterHandler.counter1()).toBe(11);
+      expect(counterHandler.counter2()).toBe(20);
+      counterHandler.counter2.increment();
+      expect(counterHandler.counter2()).toBe(21);
+    });
+  });
 });
 
-// todobefore gérer les cas quand on yield plusieurs fois le même service global/toProvide qui doivent rester la même instances & function qui doit en renvoyer plusieurs
-
+// todo later
 describe.todo(
   'injectService/ServiceToYield should expose an optional parameter that can be used to only expose what is needed and yield* dep must be used to declare non exposed fields',
-); // todo injectService/ServiceToYield with second param to only expose what is needed
+  () => {
+    it('should enable to use only some exposed field when using injectCounterExtended', () => {
+      const { injectCounterExtended } = service(
+        'CounterExtended',
+        function* () {
+          return state(10, ({ update }) => ({
+            increment: update((v) => v + 1),
+            decrement: update((v) => v - 1),
+          }));
+        },
+      );
+
+      TestBed.runInInjectionContext(() => {
+        const counterHandler = injectCounterExtended({}, (counter) => ({
+          increment: counter.increment,
+          // decrement is not exposed
+        }));
+
+        //@ts-expect-error decrement should not be accessible because it's not exposed in the second parameter of injectCounterExtended
+        counterHandler.decrement();
+
+        expect(counterHandler()).toBe(10);
+        counterHandler.increment();
+        expect(counterHandler()).toBe(11);
+      });
+    });
+
+    it('should enable to track all used fields', () => {
+      const { injectCounterExtended } = service('CounterExtended', () => {
+        return state(10, ({ update, state }) => ({
+          increment: update((v) => v + 1),
+          decrement: update((v) => v - 1),
+          state,
+        }));
+      });
+
+      TestBed.runInInjectionContext(() => {
+        const triggerDecrementObservable = new Subject<void>();
+        const counterHandler = injectCounterExtended(
+          {},
+          function* ({ state, increment, decrement }) {
+            const decrementRef = yield* decrement(); // we need to yield* it to be able to track it and know that it's used in the service, even if it's not exposed
+            triggerDecrementObservable.subscribe(() => decrementRef());
+            return {
+              state,
+              increment,
+            };
+          },
+        );
+
+        //@ts-expect-error decrement should not be accessible because it's not exposed in the second parameter of injectCounterExtended
+        counterHandler.decrement();
+
+        expect(counterHandler.state()).toBe(10);
+        counterHandler.increment();
+        expect(counterHandler.state()).toBe(11);
+        triggerDecrementObservable.next();
+        expect(counterHandler.state()).toBe(10);
+      });
+    });
+
+    it('should enable to track all used fields from ServiceToYield', () => {
+      const { CounterToYield } = service(
+        { name: 'Counter', scope: 'function' },
+        (inputs: { initialValue: MaybeSignal<number> }) =>
+          state(toValue(inputs.initialValue), ({ update }) => ({
+            increment: update((v) => v + 1),
+            decrement: update((v) => v - 1),
+            state: toValue(inputs.initialValue),
+          })),
+      );
+
+      const triggerDecrementObservable = new Subject<void>();
+
+      const { injectCounterExtended } = service(
+        'CounterExtended',
+        function* () {
+          const counter = yield* CounterToYield(
+            {
+              initialValue: signal(10),
+            },
+            function* ({ state, increment, decrement }) {
+              const decrementRef = yield* decrement(); // we need to yield* it to be able to track it and know that it's used in the service, even if it's not exposed
+              triggerDecrementObservable.subscribe(() => decrementRef());
+              return {
+                increment,
+                state,
+              };
+            },
+          );
+          return counter;
+        },
+      );
+
+      TestBed.runInInjectionContext(() => {
+        const triggerDecrementObservable = new Subject<void>();
+        const counterHandler = injectCounterExtended();
+
+        //@ts-expect-error decrement should not be accessible because it's not exposed in the second parameter of injectCounterExtended
+        counterHandler.decrement();
+
+        expect(counterHandler.state()).toBe(10);
+        counterHandler.increment();
+        expect(counterHandler.state()).toBe(11);
+        triggerDecrementObservable.next();
+        expect(counterHandler.state()).toBe(10);
+      });
+    });
+
+    it('should enable to use only some exposed field when using CounterToYield', () => {
+      const { CounterToYield } = service(
+        { name: 'Counter', scope: 'function' },
+        (inputs: { initialValue: MaybeSignal<number> }) =>
+          state(toValue(inputs.initialValue), ({ update }) => ({
+            increment: update((v) => v + 1),
+            decrement: update((v) => v - 1),
+          })),
+      );
+
+      const { injectCounterExtended } = service(
+        'CounterExtended',
+        function* () {
+          const partialCounter = yield* CounterToYield(
+            {
+              initialValue: signal(10),
+            },
+            (counter) => ({
+              state: counter,
+              increment: counter.increment,
+            }),
+          );
+
+          //@ts-expect-error decrement should not be accessible because it's not exposed in the second parameter of CounterToYield
+          partialCounter.decrement();
+
+          expect(partialCounter.state()).toBe(10);
+
+          return partialCounter;
+        },
+      );
+
+      TestBed.runInInjectionContext(() => {
+        const counterHandler = injectCounterExtended();
+        expect(counterHandler()).toBe(10);
+        counterHandler.increment();
+        expect(counterHandler()).toBe(11);
+      });
+    });
+
+    // todo later improve typing to check that only the exposed fields are accessible in the yield* and if a non exposed field is used, it should throw a typescript error
+  },
+);
 
 describe.todo('contract à implémenter pour les services');
+
+// todo later
 describe.todo('yield typage'); // with option like skipHost/optional
 // todo renvoyer une erreur si un yield d'un service à fournir est fait dans un service global (pas de host pour résoudre la dépendance)
 
